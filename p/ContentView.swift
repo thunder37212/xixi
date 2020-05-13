ef4fb9309b10232a81b6ab5624cd9d78fab02b39
陈文锡 <chenwenxi@chenwenxideMac-mini.local>
2020-05-13 10:41:50 +0800

    first time

---
+++ p/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  p
+//
+//  Created by 陈文锡 on 2020/5/12.
+//  Copyright © 2020 陈文锡. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
